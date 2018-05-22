e19a750eb0c526553c7a87d0408dfc45cb47d007
Alex McKean <amckean@me.com>
2018-05-22 15:11:50 +0000

    Added login func

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -15,8 +15,20 @@ class UserController < ApplicationController
       @user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
       @user.save
       session[:user_id] = @user.id
-      redirect '/meals' 
+      redirect '/meals'
     end
   end
+
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/meals'
+    else
+      redirect '/login'
+    end
+  end
+
 
 end
