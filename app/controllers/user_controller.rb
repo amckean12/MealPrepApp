e2ac5c7f29dcf0c849d643bd9a30b84eb4b6aa64
Alex McKean <amckean@me.com>
2018-05-24 16:34:28 +0000

    Added user auth

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,6 +4,11 @@ class UserController < ApplicationController
     erb :"/users/login"
   end
 
+  get '/logout' do
+    session.clear
+    redirect to '/'
+  end
+
   get '/signup' do
     erb :"/users/create_user"
   end
@@ -11,11 +16,15 @@ class UserController < ApplicationController
   post '/signup' do
     if params[:username]== ""  || params[:email] =="" || params[:password] == ""
       redirect "/signup"
-    else
+    #checks to make sure username does not exist in db before creating user
+    elsif User.where(:username => params[:username]).blank? && User.where(:email => params[:email]).blank?
       @user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
       @user.save
       session[:user_id] = @user.id
       redirect '/user_home'
+    #if attr exist in db then redirect to signup to create new user
+    else
+      redirect '/signup'
     end
   end
 
